5b73029b79e85432b8fdefff370e8bcd2672bbae
manojnaik18 <122071370+manojnaik18@users.noreply.github.com>
2024-12-12 22:31:15 +0530

    Create login.py

---
+++ login.py
@@ -0,0 +1,34 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+class Login:
+    def setUp(self):
+        self.driver = webdriver.Chrome()
+        self.driver.maximize_window() 
+        self.driver.get("https://sahyadri.digital/")
+    
+    def test_login(self):
+        driver = self.driver
+        driver.find_element(By.ID, "student-tab").click()
+        time.sleep(4) 
+       
+        driver.find_element(By.NAME, "admission_no").send_keys("admission no")
+        time.sleep(2)
+       
+        driver.find_element(By.CLASS_NAME, "student_pass").send_keys("password")
+        time.sleep(2)
+        
+        driver.find_element(By.ID, "login-student").click()
+        time.sleep(10) 
+
+    def tearDown(self):
+        self.driver.quit()
+
+if __name__ == "__main__":
+    test = Login()
+    test.setUp()
+    try:
+        test.test_login()
+    finally:
+        test.tearDown()
